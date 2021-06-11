3b02a997afc90fb3037b496263422d356e2a1bb9
HampusM <hampus@hampusmat.com>
2021-06-11 21:12:42 +0200

    Improved the look of the tree a little

--- packages/client/src/components/RepositoryTreeTree.vue
+++ packages/client/src/components/RepositoryTreeTree.vue
@@ -20,17 +20,19 @@
 			<tr
 				v-for="(entry, index) in tree" :key="index"
 				@click="$router.push(`/${repository}/tree${path ? '/' + path : ''}/${entry.name}`)">
-				<td class="flex-center">
-					<svg
-						xmlns="http://www.w3.org/2000/svg" height="18px"
-						viewBox="0 0 24 24" width="18px"
-						fill="#FFFFFF" v-if="entry['type'] === 'tree'"
-						preserveAspectRatio="xMidYMin">
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
-					</svg>
-					<span v-else class="tree-entry-padding" />
-					<a @click="stopClick" :href="`/${repository}/tree${path ? '/' + path : ''}/${entry.name}`">{{ entry.name }}</a>
+				<td>
+					<div class="flex-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg" height="18px"
+							viewBox="0 0 24 24" width="18px"
+							fill="#FFFFFF" v-if="entry['type'] === 'tree'"
+							preserveAspectRatio="xMidYMin">
+							<path d="M0 0h24v24H0z" fill="none" />
+							<path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
+						</svg>
+						<span v-else class="tree-entry-padding" />
+						<a @click="stopClick" :href="`/${repository}/tree${path ? '/' + path : ''}/${entry.name}`">{{ entry.name }}</a>
+					</div>
 				</td>
 				<td>
 					<a @click="routeToCommit(entry.last_commit.id, $event)" :href="`/${repository}/log/${entry.last_commit.id}`">
@@ -95,12 +97,13 @@ table {
 			}
 			td {
 				padding-bottom: 1em;
-				&:nth-child(2) a, &:nth-child(3) {
-					font-weight: 300;
-				}
 				padding-top: 5px;
 				padding-bottom: 5px;
 				padding-right: 2vw;
+				vertical-align: middle;
+				&:nth-child(2) a, &:nth-child(3) {
+					font-weight: 300;
+				}
 			}
 		}
 	}
