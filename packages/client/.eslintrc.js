d4409b9f577f04ec14bc820ea994638fd4a79687
HampusM <hampus@hampusmat.com>
2021-07-05 12:26:00 +0200

    Moved some dependencies to workspaces & fixed client eslint config

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
 	parserOptions: {
 		parser: "babel-eslint"
 	},
-	ignorePatterns: [ "/src/util/*" ],
+	ignorePatterns: [ "/src/lib/*" ],
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
