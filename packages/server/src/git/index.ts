6483962a67429b25959a61d56f1d460b2e23c951
HampusM <hampus@hampusmat.com>
2021-08-02 17:31:26 +0200

    Added documentation generation with Typedoc

---
+++ packages/server/src/git/index.ts
@@ -0,0 +1,18 @@
+/**
+ * A collection of classes that communicate with Git.
+ *
+ * @module git
+ */
+export * from "./tree";
+export * from "./branch";
+export * from "./commit";
+export * from "./diff";
+export * from "./error";
+export * from "./http";
+export * from "./blob";
+export * from "./tag";
+export * from "./patch";
+export * from "./reference";
+export * from "./repository";
+export * from "./tree_entry";
+export * from "./misc";
